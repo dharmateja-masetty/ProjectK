68494ef8d5d6f9422feaf69eefc61accd23570af
dharmateja-masetty <dharma.masetty@gmail.com>
2023-09-27 21:12:25 +0530

    Adding New Project

---
+++ App.tsx
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import ScreenSplash1 from './Screen1';
+
+
+export default function Trial() {
+
+  return (
+    <ScreenSplash1 />
+  );
+}
+
+// export default App;
