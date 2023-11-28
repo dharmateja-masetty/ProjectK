fd2ed98409ea22b01779b2fa99b86d1eff50dc02
dharmateja-masetty <dharma.masetty@gmail.com>
2023-11-28 23:54:33 +0530

    arranging files

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/home/index';
 import ProfileScreen from '../screens/profile/Profile';
+import login from '../screens/login/login';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <Tab.Navigator>
+      <Tab.Screen name="Login" component={login} />
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Products" component={ProfileScreen} />
     </Tab.Navigator>
